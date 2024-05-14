06a46a512f6a3efe2fff4b3fcb4f3d1f81d3cbed
abdelelbouhy <elbouhy@hotmail.co.uk>
2024-05-14 15:47:16 +0100

    Add same functionality using while loop

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,15 +1,27 @@
-import maxArea from ".";
+import { maxArea, maxAreaUsingWhile } from ".";
 
 describe("Calculate max area", () => {
-  it("should calculate max area from first array example", () => {
+  it("should calculate max area from first array example using brute force", () => {
     const height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
 
     expect(maxArea(height)).toEqual(49);
   });
 
-  it("should calculate max area from the second array example", () => {
-    const height = [1,1]
+  it("should calculate max area from the second array example using brute force", () => {
+    const height = [1, 1];
 
     expect(maxArea(height)).toEqual(1);
+  });
+
+  it("should calculate max area from first array example using  linear force", () => {
+    const height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+
+    expect(maxAreaUsingWhile(height)).toEqual(49);
+  });
+
+  it("should calculate max area from the second array example using  linear force", () => {
+    const height = [1, 1];
+
+    expect(maxAreaUsingWhile(height)).toEqual(1);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,34 @@
-const maxArea = (height: number[]): number => {
-    let area = 0;
-    const len = height.length
-    for (let i = 0; i < len; i++) {
-      for (let j = i + 1; j < len; j++) {
-        area = Math.max(area, Math.min(arr[j], arr[i]) * (j - i));
-      }
-    }
-    return area;
-  }
-  
-  const arr = [1, 8, 6, 2, 5, 4, 8, 3, 7];
-  console.log("Max Area: ", maxArea(arr));
+// brute force
 
-  export default maxArea;
-  
-  
+export const maxArea = (height: number[]): number => {
+  let area = 0;
+  const len = height.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      area = Math.max(area, Math.min(arr[j], arr[i]) * (j - i));
+    }
+  }
+  return area;
+};
+
+const arr = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+console.log("Max Area brute force: ", maxArea(arr));
+
+//  linear force
+export const maxAreaUsingWhile = (height: number[]): number => {
+  let i = 0;
+  let j = height.length - 1;
+  let area = 0;
+  while (i < j) {
+    area = Math.max(area, Math.min(arr[j], arr[i]) * (j - i));
+    if (height[i] < height[j]) {
+      ++i;
+    } else {
+      --j;
+    }
+  }
+  return area;
+}
+
+console.log("Max Area with linear force: ", maxAreaUsingWhile(arr));
+
